f9da2ed2cd6d748c057c6f69d5b15fd5ca8aeb26
vknayak <kumar19@navgurukul.org>
2020-05-04 11:36:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ListofCountries from "./Components/listOfCountries"
+
+function App() {
+  return (
+    <div className="App">
+    <ListofCountries />
+    </div>
+  );
+}
+
+export default App;
